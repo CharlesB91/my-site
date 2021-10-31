a36a620c6fc68379f9799d9a36a0d764f5d403c9
CharlesB91 <charles_ballantyne@hotmail.co.uk>
2021-10-31 19:32:37 +0000

    contact form now connected to emailjs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,39 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
 
-  $(document).ready(function(){
-    $('.modal').modal();
-  });
+$(document).ready(function () {
+  $(".modal").modal();
+});
 
-  $('').css('overflow', 'hidden');
+$("").css("overflow", "hidden");
+
+$("#name, #email, #textarea1").bind("keyup", function () {
+  if (allFilled()) $("#register").removeAttr("disabled");
+});
+
+function allFilled() {
+  var filled = true;
+  $("body input").each(function () {
+    if ($(this).val() == "") filled = false;
+  });
+  return filled;
+}
+
+function sendMail() {
+  emailjs
+    .send("service_gswmfxa", "template_fh1r3js", {
+      from_name: document.getElementById("first_name").value,
+      from_email: document.getElementById("email").value,
+      message: document.getElementById("textarea1").value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
+  return false; // To block from loading a new page
+}
